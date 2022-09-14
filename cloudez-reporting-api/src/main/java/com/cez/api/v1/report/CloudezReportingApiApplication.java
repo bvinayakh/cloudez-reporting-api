3e92e9a7908391528d32204ab30e2cc357ac2b95
Sree Vinayak Balasubramaniam <svinayak@8kmiles.com>
2022-09-14 11:56:34 -0700

    cors troubleshooting

--- cloudez-reporting-api/src/main/java/com/cez/api/v1/report/CloudezReportingApiApplication.java
+++ cloudez-reporting-api/src/main/java/com/cez/api/v1/report/CloudezReportingApiApplication.java
@@ -1,15 +1,11 @@
 package com.cez.api.v1.report;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -17,8 +13,6 @@ import org.springframework.web.filter.CorsFilter;
 @SpringBootApplication
 public class CloudezReportingApiApplication
 {
-  private List<String> allowedHeaders = new ArrayList<>();
-
   public static void main(String[] args)
   {
     SpringApplication.run(CloudezReportingApiApplication.class, args);
@@ -29,12 +23,17 @@ public class CloudezReportingApiApplication
   {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     CorsConfiguration config = new CorsConfiguration();
+
     config.setAllowCredentials(true);
-    config.setAllowedOriginPatterns(Arrays.asList("*"));
+
+    config.setAllowedOriginPatterns(Collections.singletonList("*"));
+
     config.setAllowedHeaders(Collections.singletonList("*"));
-    config.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(), HttpMethod.HEAD.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
-        HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name()));
+
+    config.setAllowedMethods(Collections.singletonList("*"));
+
     config.setMaxAge(1800L);
+
     source.registerCorsConfiguration("/**", config);
     FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
     bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
