f3f135a2b95cbf3527167c9602b177fb728285d6
Sree Vinayak Balasubramaniam <svinayak@8kmiles.com>
2022-09-14 14:11:53 -0700

    cors troubleshooting

--- cloudez-reporting-api/src/main/java/com/cez/api/v1/report/SecurityConfig.java
+++ cloudez-reporting-api/src/main/java/com/cez/api/v1/report/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.cez.api.v1.report;
 
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +8,9 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -24,5 +28,17 @@ public class SecurityConfig
     return http.cors().and().csrf(csrf -> csrf.disable()).authorizeRequests(auth -> {
       auth.antMatchers(PUBLIC_ENDPOINT).permitAll();
     }).httpBasic(Customizer.withDefaults()).build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource()
+  {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("*"));
+    configuration.setAllowedMethods(Arrays.asList("*"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
