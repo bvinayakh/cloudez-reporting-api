e92906711eb97eabe85f51abb64fe2ceb0bb9333
Sree Vinayak Balasubramaniam <svinayak@8kmiles.com>
2022-09-14 11:43:21 -0700

    cors fixes

--- cloudez-reporting-api/src/main/java/com/cez/api/v1/report/CloudezReportingApiApplication.java
+++ cloudez-reporting-api/src/main/java/com/cez/api/v1/report/CloudezReportingApiApplication.java
@@ -1,12 +1,15 @@
 package com.cez.api.v1.report;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -14,6 +17,8 @@ import org.springframework.web.filter.CorsFilter;
 @SpringBootApplication
 public class CloudezReportingApiApplication
 {
+  private List<String> allowedHeaders = new ArrayList<>();
+
   public static void main(String[] args)
   {
     SpringApplication.run(CloudezReportingApiApplication.class, args);
@@ -26,8 +31,10 @@ public class CloudezReportingApiApplication
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowCredentials(true);
     config.setAllowedOriginPatterns(Arrays.asList("*"));
-    config.setAllowedMethods(Collections.singletonList("*"));
     config.setAllowedHeaders(Collections.singletonList("*"));
+    config.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(), HttpMethod.HEAD.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
+        HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name()));
+    config.setMaxAge(1800L);
     source.registerCorsConfiguration("/**", config);
     FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
     bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
